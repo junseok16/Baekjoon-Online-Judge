34c81b25e263a74b909119d22e11cc97b5b36092
Junseok Tak <tjsuk0220@gmail.com>
2023-01-29 15:07:15 +0900

    Init: Create 2587번 대표값2.cpp

---
+++ Baekjoon-Online-Judge/03. 정렬(Sort)/2587번 대표값2.cpp
@@ -0,0 +1,29 @@
+﻿/*
+* @title    : 2587번 대표값2(브론즈 2)
+* @content  : 정렬
+* @author   : 탁준석
+* @date     : 230129
+* @time     : 0ms
+* @memory   : 2020KB
+* @state    : 완료
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int sum = 0;
+	vector<int> seq(5, 0);
+
+	for (int i = 0; i < 5; ++i) { cin >> seq[i]; }
+	sort(seq.begin(), seq.end());
+
+	for (const auto& e : seq) { sum += e; }
+	cout << sum / 5 << '\n' << seq[2] << '\n';
+	return 0;
+}
