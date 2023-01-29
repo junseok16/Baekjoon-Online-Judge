0da8d43a3800f80a0dad82f0d7a79f31a60aab57
Junseok Tak <tjsuk0220@gmail.com>
2023-01-29 15:07:00 +0900

    Init: Create 2750번 수 정렬하기.cpp

---
+++ Baekjoon-Online-Judge/03. 정렬(Sort)/2750번 수 정렬하기.cpp
@@ -0,0 +1,31 @@
+﻿/*
+* @title    : 2750번 수 정렬하기(브론즈 2)
+* @content  : 정렬
+* @author   : 탁준석
+* @date     : 230129
+* @time     : 0ms
+* @memory   : 2020KB
+* @state    : 완료
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+
+	vector<int> seq(N, 0);
+	for (int i = 0; i < N; ++i) { cin >> seq[i]; }
+
+	sort(seq.begin(), seq.end());
+	for (const auto& e : seq) {
+		cout << e << '\n';
+	}
+	return 0;
+}
