72866b0f52c996e8653edde8a98745d9dc89ee55
Junseok Tak <tjsuk0220@gmail.com>
2023-02-04 16:38:15 +0900

    Feat: Refactor 1197번 최소 스패닝 트리.cpp

--- Baekjoon-Online-Judge/07. 트리(Tree)/02. 최소 신장 트리(Minimum Spanning Tree)/1197번 최소 스패닝 트리.cpp
+++ Baekjoon-Online-Judge/07. 트리(Tree)/02. 최소 신장 트리(Minimum Spanning Tree)/1197번 최소 스패닝 트리.cpp
@@ -2,9 +2,9 @@
 * @title    : 1197번 최소 스패닝 트리(골드 4)
 * @content  : 최소 신장 트리
 * @author   : 탁준석
-* @date     : 230201
-* @time     : 36ms
-* @memory   : 3356KB
+* @date     : 230203
+* @time     : 56ms
+* @memory   : 7424KB
 * @state    : 완료
 */
 
@@ -12,17 +12,15 @@
 #include <vector>
 #include <algorithm>
 #include <tuple>
+#include <queue>
 using namespace std;
 
 void makeGraph();
-void kruskal();
-void initialize();
-bool isDisjoint(int x, int y);
-void Union(int x, int y);
-int Find(int x);
+void prim();
 
-vector<tuple<int, int, int>> edge;
-vector<int> parent;
+vector<vector<pair<int, int>>> adj;
+priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
+vector<bool> visited;
 
 int V, E, sumOfWeight;
 
@@ -30,58 +28,42 @@ int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
     makeGraph();
-    kruskal();
+    prim();
     return 0;
-}
-
-void initialize() {
-    for (int i = 1; i <= V; ++i) {
-        parent[i] = i;
-    }
-}
-
-void Union(int x, int y) {
-    x = Find(x);
-    y = Find(y);
-    parent[y] = x;
-}
-
-int Find(int x) {
-    if (parent[x] == x) { return x; }
-    else { return parent[x] = Find(parent[x]); }
-}
-
-bool isDisjoint(int x, int y) {
-    x = Find(x);
-    y = Find(y);
-    return x != y ? true : false;
 }
 
 void makeGraph() {
     cin >> V >> E;
-    edge = vector<tuple<int, int, int>>(E);
-    parent = vector<int>(V + 1, 0);
-    initialize();
+    adj = vector<vector<pair<int, int>>>(V + 1);
+    visited = vector<bool>(V + 1, false);
 
-    // 간선 리스트를 정의합니다.
     for (int i = 0; i < E; ++i) {
         int A, B, C;
         cin >> A >> B >> C;
-        edge[i] = { C, A, B };
+        adj[A].push_back({ C, B });
+        adj[B].push_back({ C, A });
     }
-    // 간선 리스트를 가중치를 기준으로 오름차순으로 정렬합니다.
-    sort(edge.begin(), edge.end());
 }
 
-void kruskal() {
-    for (unsigned int i = 0; i < edge.size(); ++i) {
-        // 구조적 바인딩으로 튜플 adj를 분리합니다.
-        auto [weight, from, to] = edge[i];
+void prim() {
+    for (const auto& e : adj[1]) {
+        pq.push({ e.first, 1, e.second });
+    }
+    visited[1] = true;
 
-        // 두 정점이 다른 집합에 있는 경우
-        if (isDisjoint(from, to) == true) {
-            Union(from, to);
-            sumOfWeight += weight;
+    while (!pq.empty()) {
+        auto [cost, from, to] = pq.top();
+        pq.pop();
+
+        if (visited[to] == false) {
+            visited[to] = true;
+            sumOfWeight += cost;
+
+            for (const auto& e : adj[to]) {
+                if (visited[e.second] == false) {
+                    pq.push({ e.first, to, e.second });
+                }
+            }
         }
     }
     cout << sumOfWeight << '\n';
