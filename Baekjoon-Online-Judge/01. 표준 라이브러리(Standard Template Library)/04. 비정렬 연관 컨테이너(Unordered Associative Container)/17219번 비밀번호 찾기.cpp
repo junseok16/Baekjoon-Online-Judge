989662881830b643dbba7d12cbe094662a0588a5
Junseok Tak <tjsuk0220@gmail.com>
2023-02-12 19:14:56 +0900

    Init: Create 17219번 비밀번호 찾기.cpp

---
+++ Baekjoon-Online-Judge/01. 표준 라이브러리(Standard Template Library)/04. 비정렬 연관 컨테이너(Unordered Associative Container)/17219번 비밀번호 찾기.cpp
@@ -0,0 +1,35 @@
+﻿/*
+* @title    : 17219번 비밀번호 찾기(실버 4)
+* @content  : 비정렬 연관 컨테이너
+* @author   : 탁준석
+* @date     : 230212
+* @time     : 140ms
+* @memory   : 16052KB
+* @state    : 완료
+*/
+
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+unordered_map<string, string> umap;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int N, M;
+	cin >> N >> M;
+	for (int i = 0; i < N; ++i) {
+		string site, password;
+		cin >> site >> password;
+		umap.insert({ site, password });
+	}
+
+	for (int j = 0; j < M; ++j) {
+		string site;
+		cin >> site;
+		cout << umap[site] << '\n';
+	}
+	return 0;
+}
