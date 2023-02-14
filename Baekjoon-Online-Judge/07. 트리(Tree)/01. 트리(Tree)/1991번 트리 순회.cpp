1df2d1f7f28a03f5c065d7a17ef57d680b7bb727
Junseok Tak <tjsuk0220@gmail.com>
2023-02-14 23:21:49 +0900

    Init: Create 1991번 트리 순회.cpp

---
+++ Baekjoon-Online-Judge/07. 트리(Tree)/01. 트리(Tree)/1991번 트리 순회.cpp
@@ -0,0 +1,67 @@
+﻿/*
+* @title    : 1991번 트리 순회(실버 1)
+* @content  : 트리
+* @author   : 탁준석
+* @date     : 230214
+* @time     : 0ms
+* @memory   : 2020KB
+* @state    : 완료
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void makeTree();
+void preOrder(char node);
+void inOrder(char node);
+void postOrder(char node);
+
+vector<char> leftChild, rightChild;
+
+int N;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	makeTree();
+	preOrder('A');
+	cout << '\n';
+	inOrder('A');
+	cout << '\n';
+	postOrder('A');
+	cout << '\n';
+	return 0;
+}
+
+void makeTree() {
+	cin >> N;
+	leftChild = vector<char>(N, '.');
+	rightChild = vector<char>(N, '.');
+
+	for (int i = 0; i < N; ++i) {
+		char node, left, right;
+		cin >> node >> left >> right;
+		leftChild[node - 'A'] = left;
+		rightChild[node - 'A'] = right;
+	}
+}
+
+void preOrder(char node) {
+	cout << node;
+	if (leftChild[node - 'A'] != '.') { preOrder(leftChild[node - 'A']); }
+	if (rightChild[node - 'A'] != '.') { preOrder(rightChild[node - 'A']); }
+}
+
+void inOrder(char node) {
+	if (leftChild[node - 'A'] != '.') { inOrder(leftChild[node - 'A']); }
+	cout << node;
+	if (rightChild[node - 'A'] != '.') { inOrder(rightChild[node - 'A']); }
+}
+
+void postOrder(char node) {
+	if (leftChild[node - 'A'] != '.') { postOrder(leftChild[node - 'A']); }
+	if (rightChild[node - 'A'] != '.') { postOrder(rightChild[node - 'A']); }
+	cout << node;
+}
