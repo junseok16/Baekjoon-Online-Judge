2ce97b01e501410b4b9862ff56f627655ee0fe03
Junseok Tak <tjsuk0220@gmail.com>
2023-04-21 00:49:53 +0900

    Init: Create 15657번 N과 M (8)

---
+++ Baekjoon-Online-Judge/02. 재귀(Recurstion)/02. 백트래킹(Backtrack)/15657번 N과 M (8).cpp
@@ -0,0 +1,55 @@
+/*
+* @title    : 15657번 N과 M (8)(실버 3)
+* @content  : 백트래킹
+* @author   : 탁준석
+* @date     : 230418
+* @time     : 4ms
+* @memory   : 2024KB
+* @state    : 완료
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void RepetitionCombination(unsigned int index, int begin);
+
+vector<int> sequence, set;
+
+int N, M;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N >> M;
+	sequence = vector<int>(M, 0);
+	set = vector<int>(N, 0);
+
+	for (int n = 0; n < N; ++n) {
+		cin >> set[n];
+	}
+	sort(set.begin(), set.end());
+
+	// 중복 조합을 구합니다.
+	RepetitionCombination(0, 0);
+	return 0;
+}
+
+void RepetitionCombination(unsigned int index, int begin) {
+	// 수열이 가득 찬 경우, 수열을 출력합니다.
+	if (index == sequence.size()) {
+		for (const auto& e : sequence) {
+			cout << e << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	// 수열이 가득 차지 않은 경우
+	for (int i = begin; i < N; ++i) {
+		sequence[index] = set[i];
+		RepetitionCombination(index + 1, i);
+	}
+}
